833bbdfba9627c0407eeba3d9d30560438a0c194
theaniketgiri <theaniketgiri@gmail.com>
2024-06-30 15:22:17 +0530

    m

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ mongoose.connect('mongodb+srv://Aniket:Aniket233@erp.3c3pnoq.mongodb.net/ERP?ret
 app.use(express.json());
 app.use(express.static('public'));
 
+const User = require('./models/userModel');
+async function insert(){
+  await User.create({
+    name: 'Aniket',
+    email: 'theaniketgiri@gmail.com',
+    password : 'Aniket123',
+});
 
 
+}
+insert();
 // app.use('/api/user', authRoute);
 // const authRoute = require('./routes/authRoute');
 
 
 const PORT = process.env.PORT || 3000;
+console.log(`Server is running on http://localhost:${PORT}`);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
